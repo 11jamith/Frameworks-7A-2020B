b82da34565ca91bb4b11dcd5c96687b2b3ead812
Jamith <jamithtaimal@gmail.com>
2020-09-25 22:15:25 -0500

    code_review2

--- code_review1.py
+++ code_review1.py
@@ -1,25 +1,41 @@
+#Script : Number race 
 '''
     Description:
-    randint: Let me generate integer numbers
-    uniform: Let me generate float numbers
+    randint: generar numeros enteros aleatorios 
+    uniform: generar numeros decimales aleatorios
 '''
 import os
+from os import system
 from random import randint
-#Functions:::::::::::::::::::::::::::::
+
+'para declarar una funcion se utiliza la plabra def seguida del nombre'
+#Fuctions 
 def dices () :
     d1 = randint(1,6)
     d2 = randint(1,6)
-    tot = d1 + d2
-    return [d1, d2, tot]
+    tot= d1+d2
+    #print("Dice 1: ", d1)
+    #print("Dice 2: ", d2)
+    return [d1, d2,tot]
 
-#Main::::::::::::::::::::::::::::::::::
-os.system("cls")
-i = 1
-while i <= 10 :
-    print("Tiro Nro: ", i)
+#Main :::::::::::::
+system("cls")
+dato=   int(input("ingrese numero de veces: "))
+print("",dato)
+i=1
+while i<=dato :
+    print("Tiro  Nro: ",i)
     dd = dices()
-    print("d1: ", dd[0])
-    print("d2: ", dd[1])
-    print("Total: ",dd[2])
-    i = i + 1
-    key = input("::: Lanzar nuevamente :::")
+    print("D1: ", dd[0])
+    print("D2: ", dd[1])
+    print("Total:", dd[2]) 
+    i=i+1
+    if(dd[0]==6 & dd[1]==6):
+        os._exit 
+    key= input(":::Lanzar nuevamente:::")
+
+print("******Resultados******")
+print("Valor Total de lanzamientos: ", i-1)
+
+
+
