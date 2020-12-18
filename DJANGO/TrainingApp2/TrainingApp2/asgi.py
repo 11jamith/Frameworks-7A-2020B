fd920a8266053871c99d63b86a6abf9be962178c
Jamith <jamithtaimal@gmail.com>
2020-12-17 22:43:06 -0500

    Repositorio_Final

--- DJANGO/TrainingApp2/TrainingApp2/asgi.py
+++ DJANGO/TrainingApp2/TrainingApp2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for trainingApp project.
+ASGI config for TrainingApp2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainingApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrainingApp2.settings')
 
 application = get_asgi_application()
